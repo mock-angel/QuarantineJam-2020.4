bdc6014c96e39fbe4c365b893dee0d9bbb175e46
GHZaher <53429565+GHZaher@users.noreply.github.com>
2020-05-01 16:44:18 +0300

    Tower script and bulding system

---
+++ QuarantineJam-2020.4/Assets/Scripts/TowerAttackeCommander.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAttackeCommander : MonoBehaviour
+{
+    [HideInInspector] public bool IsAttacking;
+
+    private bool hasToTargetWantedAnimal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasToTargetWantedAnimal = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetTarget(bool hasToTargetWantedAnimal)//if true then target the WantedAnimal else target the NotWantedAnimal
+    {
+        this.hasToTargetWantedAnimal = hasToTargetWantedAnimal;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!IsAttacking)
+        {
+            if (hasToTargetWantedAnimal)
+            {
+                if (collision.tag == "WantedAnimal")
+                {
+                    IsAttacking = true;
+                    print("Entered");
+                    //give shoot command to the hunters to hunt WantedAnimal
+                }
+            }
+            else
+            {
+                if (collision.tag == "NotWantedAnimal")
+                {
+                    //give shoot command to the hunters to hunt NotWantedAnimal
+                }
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (hasToTargetWantedAnimal)
+        {
+            if (collision.tag == "WantedAnimal")
+            {
+                IsAttacking = false;
+                print("Exit");
+                //give shoot command to the hunters to hunt WantedAnimal
+            }
+        }
+        else
+        {
+            if (collision.tag == "NotWantedAnimal")
+            {
+                //give shoot command to the hunters to hunt NotWantedAnimal
+            }
+        }
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (hasToTargetWantedAnimal)
+    //    {
+    //        if (collision.tag == "WantedAnimal")
+    //        {
+    //            //give shoot command to the hunters to hunt WantedAnimal
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if (collision.tag == "NotWantedAnimal")
+    //        {
+    //            //give shoot command to the hunters to hunt NotWantedAnimal
+    //        }
+    //    }
+
+    //}
+}
---
+++ QuarantineJam-2020.4/Assets/Scripts/TowerSpot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpot : MonoBehaviour
+{
+    [SerializeField] private GameObject towerPrefab;//the tower that we want to creat in this spot position
+    [SerializeField] private Vector3 positionOffset;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+        GameObject towerObj = Instantiate(towerPrefab, transform.position + positionOffset, towerPrefab.transform.rotation);
+        Tower tower = towerObj.GetComponent<Tower>();
+        tower.SpotUnderTower = this;
+    }
+}
