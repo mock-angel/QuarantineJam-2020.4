3d09f9809b1572bca6b27f1a45143ce58a5d5e34
Anantha Krishna R <sujakrishnar@gmail.com>
2020-05-01 19:05:11 +0530

    Sheep and fox spawns properly

--- QuarantineJam-2020.4/Assets/SheepFoxGeneratorScript.cs
+++ QuarantineJam-2020.4/Assets/SheepFoxGeneratorScript.cs
@@ -9,23 +9,42 @@ public class SheepFoxGeneratorScript : MonoBehaviour
     
     //amount of time before next sheep is spawned.
     public float spawnSheepTime;
+    public float foxSpawnTime;
     
-    float nextSheepSpawnTime;
+    public bool spawnFox;
+    
+    float nextSheepSpawnTime = 0;
+    float nextFoxSpawnTime = 0;
     
     // Update is called once per frame
     void Update()
     {   
-        //
-        if(nextSheepSpawnTime > Time.time){
+        if(nextSheepSpawnTime <= Time.time){
             nextSheepSpawnTime += spawnSheepTime;
             
             //spawn sheep now.
             CreateSheep();
         }
+        
+        //If spawnFox is true, spawn fox.
+        if(spawnFox && nextFoxSpawnTime <= Time.time){
+            nextFoxSpawnTime += foxSpawnTime;
+            
+            CreateFox();
+        }
     }
     
+    // Here Decide whether the sheep should go into the farm or not.
     void CreateSheep(){
         GameObject sheep = Instantiate(sheepPrefab, gameObject.transform);
         sheep.transform.position = gameObject.transform.position;
+        
+        //Decide whether sheep goes inside farm.
+        //...
+    }
+    
+    void CreateFox(){
+        GameObject fox = Instantiate(foxPrefab, gameObject.transform);
+        fox.transform.position = gameObject.transform.position;
     }
 }
