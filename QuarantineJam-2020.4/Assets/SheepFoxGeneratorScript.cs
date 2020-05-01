29387e992a7675a7b593294c054ee3dd2272b772
Anantha Krishna R <sujakrishnar@gmail.com>
2020-05-01 18:37:36 +0530

    Created untested SheepFoxGenerator script

---
+++ QuarantineJam-2020.4/Assets/SheepFoxGeneratorScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepFoxGeneratorScript : MonoBehaviour
+{
+    public GameObject sheepPrefab;
+    public GameObject foxPrefab;
+    
+    //amount of time before next sheep is spawned.
+    public float spawnSheepTime;
+    
+    float nextSheepSpawnTime;
+    
+    // Update is called once per frame
+    void Update()
+    {   
+        //
+        if(nextSheepSpawnTime > Time.time){
+            nextSheepSpawnTime += spawnSheepTime;
+            
+            //spawn sheep now.
+            CreateSheep();
+        }
+    }
+    
+    void CreateSheep(){
+        GameObject sheep = Instantiate(sheepPrefab, gameObject.transform);
+        sheep.transform.position = gameObject.transform.position;
+    }
+}
