6f4efebe8cb1582d30f4aca5f65afa528b923164
GHZaher <53429565+GHZaher@users.noreply.github.com>
2020-05-02 22:54:36 +0300

    fix #31

---
+++ QuarantineJam-2020.4/Assets/Scripts/UICanves.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICanves : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseBtn;
+    [SerializeField] private GameObject pauseImg;
+    [SerializeField] private Sprite pauseSprite;
+    [SerializeField] private Sprite playSprite;
+
+    private Image pauseBtnImg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pauseBtnImg = pauseBtn.GetComponent<Image>();
+    }
+
+    public void ChangePauseState()
+    {
+        if (Time.timeScale == 1)
+        {
+            pauseImg.SetActive(true);
+            pauseBtnImg.sprite = playSprite;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseImg.SetActive(false);
+            pauseBtnImg.sprite = pauseSprite;
+            Time.timeScale = 1;
+        }
+    }
+}
