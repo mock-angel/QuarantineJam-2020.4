07bee9a34efbe9e49876f862045e30bcb32df364
Anantha Krishna R <sujakrishnar@gmail.com>
2020-05-03 12:21:43 +0530

    Fixed the prob where MainMenu & Restart buttons callbacks were swapped

--- QuarantineJam-2020.4/Assets/Scripts/Tower.cs
+++ QuarantineJam-2020.4/Assets/Scripts/Tower.cs
@@ -10,13 +10,10 @@ public class Tower : TickObjectMonoBehaviour
     [SerializeField] private GameObject towerMenu;
     [SerializeField] private GameObject hunterImage;
     [HideInInspector] public int huntersInTower;
-    //[SerializeField] private int maxNumOfHunters;//max number of hunters in the tower
-    //[SerializeField] private int woolToUpgrade;
     
     [SerializeField] private int meatEatenPerHunterPerTick;
     
     public int initialUpgradeCost = 5;
-    int maxHunterLimit;
     int currentUpgradeCost;
    
     public TextMeshProUGUI hunterCountTxt;
@@ -29,8 +26,6 @@ public class Tower : TickObjectMonoBehaviour
     {
         huntersInTower = 1;
         currentUpgradeCost = initialUpgradeCost;
-//        StartCoroutine(ConsumeResources());
-//        resourcesManager.CreatedHunters.Add(hunters[NumOfHuntersInTheTower - 1]);
     }
     
     
@@ -39,60 +34,42 @@ public class Tower : TickObjectMonoBehaviour
         hunterCountTxt.text = huntersInTower.ToString();
     }
     
-    
-    
     public void UpgradeTower()//add more hunters to that tower
     {
         //now we first check if we have enough resources.
         
         if(ResourcesManager.Instance.SpendWool(currentUpgradeCost)){
             
-            maxHunterLimit += currentUpgradeCost;
-            
             currentUpgradeCost *= 2;
             huntersInTower++;
+            
             AudioManager.Instance.PlayUpgradeTowerAudio();
+            
             hunterImage.SetActive(true);
+            
             EventsSystem.OnUpdateResourcesCount();
         }
-        
-//        if (NumOfHuntersInTheTower < maxNumOfHunters && resourcesManager.numOfResources >= woolToUpgrade)
-//        {
-//            NumOfHuntersInTheTower++;
-//            hunters[NumOfHuntersInTheTower - 1].SetActive(true);
-////            resourcesManager.CreatedHunters.Add(hunters[NumOfHuntersInTheTower - 1]);
-////            EventsSystem.OnUpdateResourcesCount(-woolToUpgrade);
-//            
-//        }
     }
     
     public void DowngradeTower(int downGradeTimes)
     {
         for (int i = 0; i < downGradeTimes; i++)
-        {
+        
             currentUpgradeCost /= 2;
-            maxHunterLimit -= currentUpgradeCost;
-        }
     }
 
-    //public void OnClickedAddHunter(){
-        
-    //}
-    
     public void DestroyTower()//destroy the tower
     {
         Destroy(gameObject);
         SpotUnderTower.gameObject.SetActive(true);
         AudioManager.Instance.PlayDestroyTowerAudio();
 
-//        for (int i = 0; i < huntersInTower; i++)
-//        {
-////            resourcesManager.CreatedHunters.Remove(hunters[i]);
-//        }
     }
     
     public void OnClickedTower(){
+    
         SelectionManager.Instance.OnClickedTower(this);
+        
     }
     
     public void OnSelected()
@@ -114,17 +91,18 @@ public class Tower : TickObjectMonoBehaviour
     
     public override void OnTick(){
     
-        //Hunter's Lunchtime.
+        // Hunter's Lunchtime.
         int huntersCount = huntersInTower;
         ResourcesManager.Instance.EatFoodCumulative(huntersInTower, meatEatenPerHunterPerTick, out huntersInTower);
+        
         if (huntersInTower <= 0)
         {
             hunterImage.SetActive(false);
         }
+        
         if (huntersInTower < huntersCount)
         {
-            DowngradeTower(Mathf.Clamp(huntersCount - huntersInTower, 0, maxHunterLimit));
-            huntersCount = huntersInTower;
+            DowngradeTower( huntersCount - huntersInTower );
         }
     }
 }
