6af04cdaba47be9512cdb5e87b521e8e0120e8ff
Anantha Krishna R <sujakrishnar@gmail.com>
2020-05-03 09:53:38 +0530

    Made Main Menu work properly

---
+++ QuarantineJam-2020.4/Assets/Scripts/MainMenu/SwitchSceneScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class SwitchSceneScript : MonoBehaviour
+{   
+    public string mainMenuSceneName = "NormalLevel";
+    public string levelSceneName = "MainMenu";
+    
+    public void LoadLevelScene(){
+        SceneManager.LoadScene(levelSceneName);
+    }
+    
+    public void LoadMainMenuScene(){
+        SceneManager.LoadScene(mainMenuSceneName);
+    }
+}
